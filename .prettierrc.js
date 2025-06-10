3114d572b090e4828855bb9c99c76fb48126e50d
MontageWEB <394504651@qq.com>
2025-06-10 20:55:12 +0800

    chore: 配置 ESLint 和 Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false, // 不使用分号
+  singleQuote: true, // 使用单引号
+  printWidth: 100, // 每行最大长度
+  trailingComma: 'es5', // 尾随逗号
+  arrowParens: 'avoid', // 箭头函数参数括号
+  tabWidth: 2, // 缩进空格数
+  endOfLine: 'lf', // 换行符格式
+  vueIndentScriptAndStyle: true, // Vue 文件中的脚本和样式缩进
+} 
